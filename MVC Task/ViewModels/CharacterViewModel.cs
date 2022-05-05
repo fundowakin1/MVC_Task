63535726a66aee075d71496b503b57b75252859c
yehor.kalchenko <eg.kalchenko@gmail.com>
2022-05-06 01:31:41 +0200

    final fixes

--- MVC Task/ViewModels/CharacterViewModel.cs
+++ MVC Task/ViewModels/CharacterViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
 using MVC_Task.Models;
 
 namespace MVC_Task.ViewModels
@@ -18,5 +21,17 @@ namespace MVC_Task.ViewModels
         public static int PintsOfBeer { get; set; }
         public static string SpecialNpcMet { get; set; }
         public static string NpcMet { get; set; }
+
+        public static string FormatMoney()
+        {
+            var parts = AmountOfMoney.ToString("0.00", CultureInfo.InvariantCulture).Split('.').Select(int.Parse).ToArray();
+            if (parts[0] != 0 && parts[1] != 0)
+                return $"Your balance: {parts[0]} AM$ and {parts[1]} pennies.";
+            if (parts[0] == 0 && parts[1] != 0)
+                return $"Your balance: {parts[1]} pennies.";
+            if (parts[0] != 0 && parts[1] == 0)
+                return $"Your balance: {parts[0]} AM$.";
+            return AmountOfMoney.ToString();
+        }
     }
 }
